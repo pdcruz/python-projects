89a4fb484013b5bfa86dbfccb4c87e81fa15b4ee
Raoul <paridcruz@gmail.com>
2023-11-25 18:36:34 +1000

    Added answer check and score counter

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -7,12 +7,14 @@ questions = {
 }
 
 def start_quiz():
-    print("Welcome to the {} quiz show!\n".format(__name__))
+    print("\nWelcome to the {} quiz show!".format(__name__))
+    global score
+    score = 0
       
-def ask_question(q_key):
-    print(q_key)
-    generate_options(questions[q_key])
-    return input("Enter choice: ")
+def ask_question(key):
+    print("\n{}".format(key))
+    options = generate_options(questions[key])
+    return options
 
 def generate_options(option_list):
     options = {"a":"", "b":"", "c":"", "d":""}
@@ -20,8 +22,17 @@ def generate_options(option_list):
     for key in options:
         options[key] = shuffled_options.pop(0)
     print(options)
+    return (options)
+
+def check_answer(player_answer, correct_answer, options_dict):
+    return options_dict[player_answer] == correct_answer
 
 start_quiz()
 for key in questions:
-    answer = ask_question(key)
-    print(answer)
+    options = ask_question(key)
+    correct_answer = questions[key][0]
+    player_answer = input("Enter choice: ")
+    result = check_answer(player_answer, correct_answer, options)
+    print(result)
+    print(score)
+
